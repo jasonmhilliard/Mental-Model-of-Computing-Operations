720038285660eac7638e74d13d7e14fe88723dd4
Jason Hilliard <jason.m.hilliard@gmail.com>
2021-03-21 15:05:39 -0400

    Add files via upload

---
+++ basicTypeOperators.js
@@ -0,0 +1,15 @@
+/*
+Basic Types activity
+- Create 3 variables and set them up to the given instructions. Also, type check them with the typeof operator in the console.
+*/
+//your code here
+
+let myFirstName = 'Jason';
+let numberOfCities = 7;
+let havingFun = true;
+
+///////////////////////////////////
+//check results in browser console
+console.log('myFirstName: ', myFirstName);
+console.log('numberOfCities: ', numberOfCities);
+console.log('havingFun: ', havingFun);
